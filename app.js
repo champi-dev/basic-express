a1b8fce3fd00f75dbb5e795ef83e0533dec65c6a
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2019-12-16 16:59:56 -0500

    how middleware works

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const http = require('http')
 const express = require('express')
 
 const app = express()
+
+app.use((req, res, next) => {
+  console.log('In the middleware')
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('Another middleware')
+  res.send('<h1>Hello Express</h1>')
+})
+
 const server = http.createServer(app)
 
 server.listen(3000)
