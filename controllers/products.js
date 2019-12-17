e60084179d51bc38fe745ff40646c012246b8d45
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2019-12-17 16:23:36 -0500

    adding a Product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,22 @@
-const products = []
+const Product = require('../models/product')
 
 const getAddProduct = (req, res, next) => {
   res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
 }
 
 const postAddProduct = (req, res) => {
-  products.push({ title: req.body.title })
+  const product = new Product(req.body.title)
+  product.save()
   res.redirect('/')
 }
 
 const getProducts = (req, res, next) => {
+  const products = Product.fetchAll()
   res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' })
 }
 
 module.exports = {
   getAddProduct,
   postAddProduct,
-  getProducts,
-  products
+  getProducts
 }
