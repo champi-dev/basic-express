6750322752b088a306953024fd4dd6859feb1d81
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2019-12-17 08:56:08 -0500

    add express router

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
 const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express()
+
 app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use('/', (req, res, next) => {
-  console.log('This always runs')
-  next()
-})
-
-app.use('/add-product', (req, res, next) => {
-  console.log('middleware')
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title"/>
-      <button type="submit">Add product</button>
-    </form>
-  `)
-})
-
-app.post('/product', (req, res) => {
-  console.log(req.body)
-  res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-  console.log('Another middleware')
-  res.send('<h1>Hello Express</h1>')
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
 const server = http.createServer(app)
 
---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/add-product', (req, res, next) => {
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title"/>
+      <button type="submit">Add product</button>
+    </form>
+  `)
+})
+
+router.post('/product', (req, res) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+module.exports = router
