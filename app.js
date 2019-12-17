45ac9c09f50be9556f6993048866c46f7395319f
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2019-12-17 08:59:57 -0500

    add a 404 page

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(adminRoutes)
 app.use(shopRoutes)
+app.use((req, res, next) => {
+  res.status(404).send('<h1>page not found</h1>')
+})
 
 const server = http.createServer(app)
 
