29761fd925919098c993c15ca4af30d2e52cff9b
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2019-12-17 09:44:24 -0500

    using a helper function for navigation

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http')
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const rootDir = require('./utils/path')
 
 const app = express()
 
@@ -10,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(adminRoutes)
 app.use(shopRoutes)
 app.use((req, res, next) => {
-  res.status(404).send('<h1>page not found</h1>')
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 const server = http.createServer(app)
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
+const rootDir = require('../utils/path')
 
 router.get('/admin/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 router.post('/admin/add-product', (req, res) => {
