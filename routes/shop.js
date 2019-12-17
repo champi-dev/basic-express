0a200887e4ea70d957d80de2d1623de9cf3bde0d
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2019-12-17 11:46:18 -0500

    outputting dynamic content

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,7 @@ const { products } = require('./admin')
 const rootDir = require('../utils/path')
 
 router.get('/', (req, res, next) => {
-  res.render('shop')
+  res.render('shop', { prods: products, docTitle: 'Shop' })
 })
 
 module.exports = router
