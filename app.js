6cd4dfcf3d6bd6077e811e4a316452433d97a4ee
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2019-12-17 14:16:52 -0500

    working with ejs

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const http = require('http')
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const expressHbs = require('express-handlebars')
+
 const { routes: adminRoutes, products } = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const rootDir = require('./utils/path')
 
 const app = express()
 
-app.set('view engine', 'pug')
+// app.engine('hbs', expressHbs())
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
